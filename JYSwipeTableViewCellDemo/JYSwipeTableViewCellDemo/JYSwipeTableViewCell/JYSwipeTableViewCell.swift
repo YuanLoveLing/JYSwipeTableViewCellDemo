d39cd33da835b4ed984c15e1cd70d27144256759
Jhonny Jin <736010695@qq.com>
2017-05-08 17:20:50 +0800

    初始化完毕

---
+++ JYSwipeTableViewCellDemo/JYSwipeTableViewCellDemo/JYSwipeTableViewCell/JYSwipeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  JYSwipeTableViewCell.swift
+//  JYSwipeTableViewCellDemo
+//
+//  Created by 靳志远 on 2017/5/8.
+//  Copyright © 2017年 靳志远. All rights reserved.
+//
+
+import UIKit
+
+class JYSwipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ JYSwipeTableViewCellDemo/JYSwipeTableViewCellDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JYSwipeTableViewCellDemo
+//
+//  Created by 靳志远 on 2017/5/8.
+//  Copyright © 2017年 靳志远. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
